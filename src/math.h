e037b20869a14248c1c498f043ee9f3a5032ade5
wjpjw <recvfromsockaddr@gmail.com>
2018-12-01 16:59:41 +0800

    w

---
+++ src/math.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "internal.h"
+
+#define posixc_min(a,b) (((a)<(b))?(a):(b))
+
+#define posixc_max(a,b) (((a)>(b))?(a):(b))
+
--- src/slice.h
+++ src/slice.h
@@ -6,3 +6,4 @@ void posixc_slice_init(posixc_slice*slice, uint64_t size);
 void posixc_slice_destroy(posixc_slice*slice);
 void posixc_slice_init_from(posixc_slice*slice, const char* str);
 void posixc_slice_copy(posixc_slice*dst, posixc_slice*src);
+bool posixc_slice_contains(posic_slice*slice);

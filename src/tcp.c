a6875c7ea664b4b054216de58a4a361cda828d41
wjpjw <recvfromsockaddr@gmail.com>
2018-12-02 23:33:30 +0800

    w

--- src/tcp.c
+++ src/tcp.c
@@ -1,4 +1,59 @@
 #pragma once
 
-#include "internal.h"
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include "tcp.h"
+#include "common/slice.h"
+
+int posixc_tcp_socketfd(){
+    return socket(AF_INET, SOCK_STREAM, 0);
+}
+
+void posixc_tcp_set_addr(struct sockaddr_in* sin, const char* host, int port){
+    sin->sin_family = AF_INET;
+    sin->sin_addr.s_addr = inet_addr(host);
+    sin->sin_port = htons(port);
+}
+
+void posixc_tcp_set_addr_any(struct sockaddr_in* sin, int port){
+    sin->sin_family = AF_INET;
+    sin->sin_addr.s_addr = htonl(INADDR_ANY);
+    sin->sin_port = htons(port);
+}
+
+bool posixc_tcp_is_addr_valid(struct sockaddr_in* sin){
+    return sin->sin_addr.s_addr != INADDR_NONE;
+}
+
+int posixc_tcp_connect(int fd, struct sockaddr_in* sin){
+    return connect(fd, sin, sizeof(*sin));
+}
+
+int posixc_tcp_bind(int fd, struct sockaddr_in* sin){
+    return bind(fd, sin, sizeof(*sin));
+}
+
+int posixc_tcp_listen(int fd){
+    return listen(fd,1);
+}
+
+int posixc_tcp_accept_fd(int fd, struct sockaddr_in* cin)
+    socklen_t cin_sz = sizeof(*cin);
+    return accept(fd, cin, &cin_sz);
+}
+
+posixc_slice posixc_tcp_get_ip(struct sockaddr_in* cin){
+    posixc_slice s;
+    posixc_slice_init_from(&s, inet_ntoa(cin->sin_addr))
+    return s;
+}
+
+int posixc_tcp_get_port(struct sockaddr_in* cin){
+    return (int)ntohs(cin->sin_port));
+}
 
--- src/tcp.h
+++ src/tcp.h
@@ -2,20 +2,12 @@
 
 #include "internal.h"
 
-int posixc_tcpipv4_socket(){
-    return socket(AF_INET, SOCK_STREAM, 0);
-}
-
-void posixc_set_remote_addr(){
-    struct sockaddr_in sin;
-    sin.sin_family = AF_INET;
-    sin.sin_addr.s_addr = inet_addr(server);
-    sin.sin_port = htons(port);
-
-    if (sin.sin_addr.s_addr == INADDR_NONE) {
-        return;
-        printf("invalid remote IP %s\n", server);
-        exit(-1);
-    }
-
-}
+int posixc_tcp_socketfd(); //get tcp ipv4 socket fd
+void posixc_tcp_set_addr(struct sockaddr_in* sin, const char* host, int port);
+bool posixc_tcp_is_addr_valid(struct sockaddr_in* sin);
+int posixc_tcp_connect(int fd, struct sockaddr_in* sin);
+int posixc_tcp_bind(int fd, struct sockaddr_in* sin);
+int posixc_tcp_listen(int fd);
+int posixc_tcp_accept_fd(int fd, struct sockaddr_in* cin); //get accepted fd
+posixc_slice posixc_tcp_get_ip(struct sockaddr_in* cin);
+int posixc_tcp_get_port(struct sockaddr_in* cin); 
